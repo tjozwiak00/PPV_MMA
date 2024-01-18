8c74f49348b70ba2fb03111a34dd76b82d894168
tjozwiak00 <tomek.jozwiak00@gmail.com>
2024-01-18 14:22:45 +0100

    Jakiś błąd naprawiony

--- mmappv1/Models/HIstoriaZakupów.cs
+++ mmappv1/Models/HIstoriaZakupów.cs
@@ -7,6 +7,6 @@ namespace mmappv1.Models
     public class PurchaseHistory
     {
         public int Id { get; set; }
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
     }
 }
